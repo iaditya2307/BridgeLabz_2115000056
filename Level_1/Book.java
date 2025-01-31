5fd18adffe4265ea685c6b5bdb9ccb5f9f99d491
Aditya Chauhan <adityachauhan.ac2307@gmail.com>
2025-01-31 11:49:32 +0530

    added package name to files

--- Level_1/Book.java
+++ Level_1/Book.java
@@ -1,3 +1,6 @@
+
+package Level_1;
+
 import java.util.*;
 
 public class Book {
@@ -10,13 +13,30 @@ public class Book {
         this.author = author;
         this.price = price;
     }
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-    public String getAuthor() { return author; }
-    public void setAuthor(String author) { this.author = author; }
-    public double getPrice() { return price; }
-    public void setPrice(double price) { this.price = price; }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
     public void displayDetails() {
         System.out.println("Book Details:");
--- Level_1/Employee.java
+++ Level_1/Employee.java
@@ -1,37 +1,49 @@
+package Level_1;
+
 import java.util.*;
+
 public class Employee {
     private String name;
     private int id;
     private double salary;
+
     public Employee(String name, int id, double salary) {
         this.name = name;
         this.id = id;
         this.salary = salary;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public double getSalary() {
         return salary;
     }
+
     public void setSalary(double salary) {
         this.salary = salary;
     }
+
     public void displayDetails() {
         System.out.println("Employee Details:");
         System.out.println("Name: " + name);
         System.out.println("ID: " + id);
         System.out.println("Salary: $" + salary); // Added $ for currency
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter details for employee 1:");
@@ -50,7 +62,7 @@ public class Employee {
         int id2 = scanner.nextInt();
         System.out.print("Salary: ");
         double salary2 = scanner.nextDouble();
-        scanner.nextLine(); 
+        scanner.nextLine();
         Employee employee2 = new Employee(name2, id2, salary2);
         employee1.displayDetails();
         System.out.println("------------------");
